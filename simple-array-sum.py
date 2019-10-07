3d7579500cc110902e5f77629c3ad8f2ee3cbeda
Kylema <kylemaa95@gmail.com>
2019-10-07 14:57:00 -0700

    solved problems from last week

---
+++ simple-array-sum.py
@@ -0,0 +1,29 @@
+#https://www.hackerrank.com/challenges/simple-array-sum/problem
+
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the simpleArraySum function below.
+#
+def simpleArraySum(ar):
+    #
+    # Write your code here.
+    #
+    return sum(ar)
+
+
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+    
