d9b12034a4c9a206602890a555f45616881613e9
Kylema <kylemaa95@gmail.com>
2019-10-09 13:09:52 -0700

    Mistake: counted first before set(ar)

--- sock-merchant.py
+++ sock-merchant.py
@@ -1,21 +1,14 @@
 # https://www.hackerrank.com/challenges/sock-merchant/problem?h_r=next-challenge&h_v=zen#
 # Complete the sockMerchant function below.
 def sockMerchant(n, ar):
-    count = 0 
-    holder = []
-    for i in ar: 
-        holder.append(ar.count(i))
-    holder = list(dict.fromkeys(holder))
-    for j in holder:
-        if j == 2:
-            count += 1 
-        elif j > 2:
-            count += (j//2)
-    return count 
+    some_num = sum(ar.count(x)//2 for x in set (ar))
+    return some_num 
     
-
+    
     n = int(input())
 
     ar = list(map(int, input().rstrip().split()))
 
     result = sockMerchant(n, ar)
+
+    print(result)
