d7537cd24c6bdb4f0afa36a5a10e3e3fe0d1945e
Kylema <kylemaa95@gmail.com>
2019-10-09 13:11:56 -0700

    Updating

---
+++ diagonal-difference.py
@@ -0,0 +1,27 @@
+# https://www.hackerrank.com/challenges/diagonal-difference/problem
+import os
+
+def diagonalDifference(arr):
+    # Write your code here
+    # Complete this function  
+    sum1 = sum2 = 0 
+    for i in range(len(arr)):
+         sum1 += int(arr[i][i]) # arr[row][column]
+         sum2 += int(arr[i][n-i-1])
+    return abs(sum1-sum2)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
