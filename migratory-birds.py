9c61412db5580cd7fe1875f799691d30b45e48b6
Kylema <kylemaa95@gmail.com>
2019-10-14 12:34:52 -0700

    fixed compile error by allocating space for res varible

---
+++ migratory-birds.py
@@ -0,0 +1,29 @@
+# https://www.hackerrank.com/challenges/migratory-birds/problem
+import math
+import os
+import random
+import re
+import sys
+
+def migratoryBirds(arr):
+    holder = [0]*6
+    for i in arr:
+        holder[i] += 1
+    
+    res = holder.index(max(holder))
+    return res
+
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
