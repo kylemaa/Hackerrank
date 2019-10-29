cf1dca309bd0f6a3d02c1d64ab2c5cc648300b33
Kylema <kylemaa95@gmail.com>
2019-10-29 10:11:27 -0700

    Day 9th of 30DoC

---
+++ 30-Days-of-Code/Day9-recursion.py
@@ -0,0 +1,27 @@
+# https://www.hackerrank.com/challenges/30-recursion/problem
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the factorial function below.
+def factorial(n):
+    if n > 1:
+        result = n * factorial(n - 1)
+        return result
+    else:
+        return 1
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    result = factorial(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
