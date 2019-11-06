fa0c107c4f2e0e4a41aab0b5040ba9d3f1e95a92
Kylema <kylemaa95@gmail.com>
2019-11-06 12:32:44 -0800

    Daily prob

---
+++ Implementation/birthday-chocolate.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/the-birthday-bar/problem
+def birthday(s, d, m):
+    res = 0 
+    for i in range (len(s)):
+        if sum (s[i:m+i]) == d: 
+            res += 1
+    return res 
+
