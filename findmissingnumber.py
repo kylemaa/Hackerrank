bb62c7e4246b3e7245a1f93594742370def17b56
Kylema <kylemaa95@gmail.com>
2019-11-19 13:44:12 -0800

    function is returning 0, not going into loop

--- findmissingnumber.py
+++ findmissingnumber.py
@@ -1,17 +1,18 @@
 # Given an array A of N integers, return that smallest positive integer that does not occur in A. 
 import os
 def solution(A):
+    A = sorted(A)
+    smallest = 0
     for i in range(len(A)-1):
-        print(A[i])
-        if A[i] >= 0 and A[i] != A[i+1] and A[i] != A[i+1] + 1: 
-            smallest = A[i]+1
-            return smallest 
-        else:
-            smallest = A[-1]+1
+        if A[i] >= 0:
+            if A[i] != A[i+1] and A[i] != A[i+1] + 1: 
+                smallest = A[i]+1
+            else:
+                smallest = A[-1]+1
     return smallest
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+if __name__ == "__main__":
+    pass
 
     n = int(input().strip())
 
@@ -21,7 +22,4 @@ if __name__ == '__main__':
         A.append(list(map(int, input().rstrip().split())))
     
     result = solution(A)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+    print(result)
