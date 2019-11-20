f19576d6e69d4389ffd79457efdef42e6795f140
Kylema <kylemaa95@gmail.com>
2019-11-19 20:44:39 -0800

    fixed and solved with sorted, not optimal

--- Screening/find-missing-number.py
+++ Screening/find-missing-number.py
@@ -1,25 +1,18 @@
 # Given an array A of N integers, return that smallest positive integer that does not occur in A. 
 import os
-def solution(A):
-    A = sorted(A)
+def solution(arr):
+    a = sorted(arr)
+    print(a)
     smallest = 0
     for i in range(len(A)-1):
         if A[i] > 0:
-            if A[i] != A[i+1] and A[i] != A[i+1] + 1: 
+            if A[i] != A[i+1] and A[i]+1 != A[i+1]: 
                 smallest = A[i]+1
                 return smallest 
             # I can check this at the beginning. 
-    return smallest
+            else:
+                return A[-1]+1
 
-if __name__ == "__main__":
-    pass
 
-    n = int(input().strip())
-
-    A = []
-
-    for _ in range(n):
-        A.append(list(map(int, input().rstrip().split())))
-    
-    result = solution(A)
-    print(result)
+A = [1, 2, 6, 4]
+print(solution(A))
