b3ec21bd083c8c42d7b4a31db74baca89d3e0672
Kylema <kylemaa95@gmail.com>
2019-11-19 20:50:04 -0800

    fixed syntax and solved with sorted, not optimal

--- Screening/find-missing-number.py
+++ Screening/find-missing-number.py
@@ -4,15 +4,15 @@ def solution(arr):
     a = sorted(arr)
     print(a)
     smallest = 0
-    for i in range(len(A)-1):
-        if A[i] > 0:
+    for i in range(len(A)):
+        if A[i] > 0 and i < len(A)-1:
             if A[i] != A[i+1] and A[i]+1 != A[i+1]: 
                 smallest = A[i]+1
                 return smallest 
             # I can check this at the beginning. 
             else:
-                return A[-1]+1
+                return a[-1]+1
 
 
-A = [1, 2, 6, 4]
+A = [1, 2, 5, 4]
 print(solution(A))
