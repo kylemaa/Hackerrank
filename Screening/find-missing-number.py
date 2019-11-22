409491e2e257d2e7ddb323c344518a411ab75382
Kylema <kylemaa95@gmail.com>
2019-11-22 09:07:06 -0800

    using memoization, instead of sorting

--- Screening/find-missing-number.py
+++ Screening/find-missing-number.py
@@ -1,18 +1,23 @@
 # Given an array A of N integers, return that smallest positive integer that does not occur in A. 
 import os
 def solution(arr):
-    a = sorted(arr)
-    print(a)
-    smallest = 0
-    for i in range(len(A)):
-        if A[i] > 0 and i < len(A)-1:
-            if A[i] != A[i+1] and A[i]+1 != A[i+1]: 
-                smallest = A[i]+1
-                return smallest 
-            # I can check this at the beginning. 
-            else:
-                return a[-1]+1
-
+    m = max(arr)
+    if m < 1:
+        return 1
+    if len(arr)== 1:
+        if arr[0] == 1:
+            return 2
+        else:
+            return 1
+    l = [0]* m 
+    for i in range(len(arr)):
+        if arr[i]>0:
+            if l[arr[i]] !=1:
+                l[arr[i]] = 1
+    for j in range(len(l)):
+        if l[j] == 0:
+            return i+1
+    return i+2
 
 A = [1, 2, 5, 4]
 print(solution(A))
