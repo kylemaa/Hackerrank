0637a2b753d8ea8e6e2eb51a68a2400552f7eb1f
kylemaa <kylephuma@gmail.com>
2019-11-28 20:10:18 -0800

    recursive method helper()

---
+++ LeetCode/add-two-numbers-as-linked-list.py
@@ -0,0 +1,36 @@
+class Node(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        # param of two lists with init value 0 
+        return self.helper(l1, l2, 0)
+    # this function update the carry over and return the next links if next is not null    
+    def helper(self, l1, l2, c):
+        val = l1.val + l1.val + c
+        c = val//10
+        ret = Node(val % 10)
+        if l1.next != None and l2.next != None:
+            if not l1.next:
+                l1.next = Node(0)
+            if not l2.next:
+                l2.next = Node(0)    
+            ret.next = self.helper(l1.next, l2.next, c)
+        return ret
+
+
+l1 = Node(2)
+l1.next = Node(4)
+l1.next.next = Node(3)
+
+l2 = Node(5)
+l2.next = Node(6)
+l2.next.next = Node(4)
+
+answer = Solution().addTwoNumbers(l1,l2)
+while answer: 
+    # print the node and its links 
+    print(answer.val)
+    answer = answer.next
