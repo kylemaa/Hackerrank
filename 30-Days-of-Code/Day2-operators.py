64be3a01080b54f44334099560d223c74a0b7d0d
kylemaa <kylephuma@gmail.com>
2019-11-30 16:25:08 -0800

    s update

--- 30-Days-of-Code/Day2-operators.py
+++ 30-Days-of-Code/Day2-operators.py
@@ -14,7 +14,7 @@ def solve(meal_cost, tip_percent, tax_percent):
     tip = meal_cost*(tip_percent/100)
     tax = meal_cost*(tax_percent/100)
     total = meal_cost + tip +tax
-    print (round(total))
+    print(round(total))
 
 if __name__ == '__main__':
     meal_cost = float(input())
