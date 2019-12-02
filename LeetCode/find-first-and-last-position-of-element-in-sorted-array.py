4171f15f6d988dd10aa98cff5b1e76a3e6762d61
Kyle Ma <32466847+kylemaa@users.noreply.github.com>
2019-12-02 12:46:08 -0800

    Update find-first-and-last-position-of-element-in-sorted-array.py

--- LeetCode/find-first-and-last-position-of-element-in-sorted-array.py
+++ LeetCode/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,2 +1,10 @@
 # Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
 # Because the array is already sorted, we can use binary search tree for solve for logarithmic time. 
+class Solution:
+    # returns leftmost (or rightmost) index at which `target` should be inserted in sorted
+    # array `nums` via binary search.
+    def extreme_insertion_index(self, nums, target, left):
+      lo = 0
+      hi = len(nums)
+      while 
+
