656e3a26b48244d441e18ea6ccb41ee44707f53b
kylemaa <kylephuma@gmail.com>
2019-12-03 07:27:40 -0800

    Linear time, constant space method

--- LeetCode/sort-3-numbers.py
+++ LeetCode/sort-3-numbers.py
@@ -5,5 +5,18 @@ def sortNums(nums):
         count[n] = count.get(n, 0) + 1
     return ([999]* count.get(1,0) + [2]* count.get(2,0) + [3]* count.get(3,0))
 
+def sortNums2(nums):
+    start = 0 
+    end = len(nums)
+    index = 0 
+    while index <= end:
+        if nums[index] == 1:
+            # swap
+            start += 1 
+            index += 1
+        if nums[index] == 2:
+            # do nothing
+        if nums[index] == 3:
+            # swap
 
 print(sortNums ([3, 3, 2, 1, 3, 2, 1, 1]))
