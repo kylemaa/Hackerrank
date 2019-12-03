45f49b7aef4275644196637ae88e6a58944f60a2
kylemaa <kylephuma@gmail.com>
2019-12-03 07:42:16 -0800

    Linear time, constant space method

--- LeetCode/sort-3-numbers.py
+++ LeetCode/sort-3-numbers.py
@@ -1,3 +1,4 @@
+# linear time with O(n + m), n be the size of nums and m be the size of dictionary
 def sortNums(nums):
     count = {}
     for n in nums:
@@ -5,18 +6,24 @@ def sortNums(nums):
         count[n] = count.get(n, 0) + 1
     return ([999]* count.get(1,0) + [2]* count.get(2,0) + [3]* count.get(3,0))
 
+# linear time with O(n), in this algorithm brute force swapping the known elements.
 def sortNums2(nums):
     start = 0 
-    end = len(nums)
+    end = len(nums) -1 
     index = 0 
     while index <= end:
         if nums[index] == 1:
             # swap
+            nums[index], nums[start]= nums[start], nums[index]
             start += 1 
             index += 1
-        if nums[index] == 2:
+        elif nums[index] == 2:
             # do nothing
-        if nums[index] == 3:
+            index += 1
+        elif nums[index] == 3:
             # swap
+            nums[index], nums[end]= nums[end], nums[index]
+            end -= 1 
+    return nums
 
-print(sortNums ([3, 3, 2, 1, 3, 2, 1, 1]))
+print(sortNums2([3, 3, 2, 1, 3, 2, 1, 1]))
