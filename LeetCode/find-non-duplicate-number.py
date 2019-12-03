9231e37ae3cb1a95c6e03873bc658d3994226e0b
kylemaa <kylephuma@gmail.com>
2019-12-03 15:45:43 -0800

    XOR trick

--- LeetCode/find-non-duplicate-number.py
+++ LeetCode/find-non-duplicate-number.py
@@ -1,7 +1,7 @@
 # https://leetcode.com/problems/single-number/
 class Solution:
     # with n being the length of nums and m being the length of dictionary 
-    # this solution has the time complexity of O(n + m)
+    # this solution has the time complexity of O(n + m) which is linear time O(n)
     def singleNumber(self, nums):
         occurrence = {}
         for n in nums:
@@ -9,9 +9,19 @@ class Solution:
                 occurrence[n] += 1 
             except KeyError:
                 occurrence[n] = 1
-        for x in occurrence:
-            if occurrence[x] == 1:
-                return x
+        # use .items() to iterating 
+        for k, v in occurrence.items():
+            if v == 1: 
+                return k
+            
+
+    # to improve the space complexity we can use XOR op to cancle out the duplicates
+    def singleNumber2(self, nums):
+        unique = 0 
+        for n in nums:
+            unique ^= n
+            print(unique)
+        return unique
 
 
-print(Solution().singleNumber([4, 3, 2, 4, 1, 3, 2]))
+print(Solution().singleNumber2([4, 3, 2, 4, 1, 3, 2]))
