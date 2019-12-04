bb6d228880b74ebfc54657758095f78385682851
kylemaa <kylephuma@gmail.com>
2019-12-03 19:08:47 -0800

    dont understand the return-outside-function error

---
+++ LeetCode/course-schedule.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/course-schedule/
+# this is a graph problem with adjacency lists 
+from collections import defaultdict
+# prerequisties is a list of sets of two integers
+class Solution:
+    def canFinish(self, numCourses, prerequisites):
+        self.graph = defaultdict(list)
+        for edge in prerequisites:
+            self.graph[edge[0]].append(edge[1])
+            
+        visited = set()
+
+    # True if there is a cycle, False if not
+    def visit(self, vertex):
+      visited.add(vertex)
+      for neighbour in self.graph[vertex]:
+        if neighbour in self.visited or self.visit(neighbour):
+          return True
+      self.visited.remove(vertex)
+      return False
+
+
+    for i in range(numCourses):
+        if visit(i):
+            return False
+        return True
+
---
+++ LeetCode/find-max-stack.py
@@ -0,0 +1,34 @@
+class MaxStack(object):
+    # maxes stack holds the current max value.
+    def __init__(self, *args, **kwargs):
+        self.stack = []
+        self.maxes = []
+
+    def push(self,val):
+        self.stack.append(val)
+        if self.maxes and self.maxes[-1]> val:
+            self.maxes.append(self.maxes[-1])
+        else:
+            self.maxes.append(val)
+
+    def pop(self):
+        if self.maxes:
+            self.maxes.pop()
+        return self.stack.pop()
+
+    def max(self):
+        return self.maxes[-1]
+
+s = MaxStack()
+s.push(1)
+s.push(2)
+s.push(3)
+s.push(2)
+print('max', s.max())
+print(s.pop())
+print('max', s.max())
+print(s.pop())
+print('max', s.max())
+print(s.pop())
+print('max', s.max())
+print(s.pop())
