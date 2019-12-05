67aa4d5c57b82a5028ce91a859295516c94b0d05
kylemaa <kylephuma@gmail.com>
2019-12-05 14:48:25 -0800

    by not counting the dips, we can use dip as index of the array to make statements

--- LeetCode/non-decreasing-array.py
+++ LeetCode/non-decreasing-array.py
@@ -6,15 +6,20 @@ class Solution:
         dips = None
         for i in range(len(nums)-1):
             if nums[i] > nums[i+1]:
+                # if dips more than once
                 if dips is not None:
-                    dips = i
+                    return False
+            dips = i
         # if there's no dip
         if dips is None:
             return True
+        # if there's one dip at first index
         if dips == 0: 
             return True
+        # if there's one dip next to last
         if dips == len(nums) -2:
             return True 
+        # if number at that dip smaller than the next two indexes or the other case
         if nums[dips] <= nums[dips + 2] or nums[dips - 1] <= nums[dips + 1]:
             return True
         return False
