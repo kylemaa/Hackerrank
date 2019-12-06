4fe78236a51a96aaecb138fb31b42f99e6071abd
kylemaa <kylephuma@gmail.com>
2019-12-05 21:50:23 -0800

    comment

--- LeetCode/word-search.py
+++ LeetCode/word-search.py
@@ -1,7 +1,8 @@
 class Grid(object):
     def __init__(self, matrix):
         self.matrix = matrix
-
+    
+    # main function to iterate through each row and column element
     def wordSearch(self, word):
         for i in range(len(matrix)):
             if self.wordSearchRight(i, word):
@@ -10,14 +11,14 @@ class Grid(object):
             if self.wordSearchDown(i, word):
                 return True
         return False
-            
 
+    # helper function to search for to the right of each row 
     def wordSearchRight(self,index, word):
         for i in range(len(self.matrix[index])):
             if word[i] != matrix[i][index]:
                 return False 
         return True
-
+    # helper function to search for word downward of each column
     def wordSearchDown(self,index, word):
         for i in range(len(self.matrix[index])):
             if word[i] != matrix[index][i]:
@@ -31,4 +32,5 @@ matrix = [
     ['A', 'N', 'O', 'B'],
     ['M', 'A', 'S', 'S']]
 
+# printing class object with method 
 print(Grid(matrix).wordSearch('FACI'))
