1805c3d74f05f695465364bdf100f4e1a608e8dc
kylemaa <kylephuma@gmail.com>
2019-12-07 09:50:15 -0800

    did

---
+++ LeetCode/valid-paratheses.py
@@ -0,0 +1,44 @@
+# https://leetcode.com/problems/valid-parentheses/
+
+def validParantheses(s):
+    stack = []
+    for c in s:
+        if c == '(':
+            stack.append('(')
+        if c == ')':
+            if len(stack) == 0:
+                return False
+            if stack[-1] != '(':
+                return False
+            else:
+                stack.pop()
+
+        if c == '[':
+            stack.append('[')
+        if c == ']':
+            if len(stack) == 0:
+                return False
+            if stack[-1] != '[':
+                return False
+            else:
+                stack.pop()
+        
+        if c == '{':
+            stack.append('{')
+        if c == '}':
+            if len(stack) == 0:
+                return False
+            if stack[-1] != '{':
+                return False
+            else:
+                stack.pop()
+        
+        #egde case 
+    if len(stack)>0:
+        return False
+    else:
+        return True
+
+
+s = '()[]'
+print(validParantheses(s))
