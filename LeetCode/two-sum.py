97347e23cbe3824a3197f9394a0f89c9ba70913b
kylemaa <kylephuma@gmail.com>
2019-12-11 20:07:35 -0800

    reviewing

--- LeetCode/two-sum.py
+++ LeetCode/two-sum.py
@@ -1,21 +1,23 @@
 def twoSumBrute(arr, target):
     # Using nested iterative loop to match each element with one another
     for i in range(len(arr)):
-        for j in range(1,len(arr)):
+        for j in range(1, len(arr)):
             if arr[i] + arr[j] == target:
-                return [i,j]
+                return [i, j]
+
+
 def twoSumHash(arr, target):
     #table = {i:v for i, v in enumerate(arr)}
     table = {}
     for i, v in enumerate(arr):
-        complement = target - v 
+        complement = target - v
         if complement in table:
             return [i, table[complement]]
-        #if complement not in table:
+        # if complement not in table:
         table[v] = i
 
-    
+
 target = 6
 arr = [3, 3, 5, 6]
-#print ('brute:', twoSumBrute(arr,target))
-print ('hash:', twoSumHash(arr,target))
+print('brute:', twoSumBrute(arr, target))
+print('hash:', twoSumHash(arr, target))
