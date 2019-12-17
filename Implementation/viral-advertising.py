f08aa6e569126e8b7a7b481b0018997229c8196c
kylemaa <kylephuma@gmail.com>
2019-12-16 22:28:56 -0800

    proficiency

--- Implementation/viral-advertising.py
+++ Implementation/viral-advertising.py
@@ -8,14 +8,17 @@ import re
 import sys
 
 # Complete the viralAdvertising function below.
+
+
 def viralAdvertising(n):
-    shared = 5 
+    shared = 5
     likes = 0
-    for i in range(n):
-        likes += shared//2 
+    for _ in range(n):
+        likes += shared//2
         shared = (shared//2) * 3
-    
+
     return likes
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
