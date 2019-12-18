f668674f163df8ffef600a24dc1ed81942cbe740
kylemaa <kylephuma@gmail.com>
2019-12-18 13:56:07 -0800

    set methods

---
+++ Python-Proficiency/set.add().py
@@ -0,0 +1,14 @@
+# https://www.hackerrank.com/challenges/py-set-add/problem
+
+# num = int(input())
+# dist = set()
+# for i in range(num)):
+#     dist.add(str(input())
+
+# print(len(dist))
+
+
+s = set()
+for i in range(int(input())):
+    s.add(input())
+print(len(s))
