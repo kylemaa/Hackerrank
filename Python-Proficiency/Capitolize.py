d0ea5d615b667442545e06e3acc1d31e6bd0d492
kylemaa <kylephuma@gmail.com>
2019-12-23 20:16:43 -0800

    a small problem

--- Python-Proficiency/Capitolize.py
+++ Python-Proficiency/Capitolize.py
@@ -1,4 +1,17 @@
-s = input()
-for x in s[:].split():
-    s = s.replace(x, x.capitalize())
-print(s)
+def solve(s):
+    s = input()
+    for x in s[:].split():
+        s = s.replace(x, x.capitalize())
+    print(s)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
