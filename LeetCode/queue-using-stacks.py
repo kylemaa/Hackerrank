a6d8303e2b123cfe903d728ae6dd3c04a8f32df9
kylemaa <kylephuma@gmail.com>
2020-01-15 21:46:09 -0800

    understanding the difference of stacks and queues

--- LeetCode/queue-using-stacks.py
+++ LeetCode/queue-using-stacks.py
@@ -2,19 +2,20 @@ class Queue(object):
     def __init__(self):
         self.s1 = []
         self.s2 = []
+    # this method would push the val into the first stack
 
     def enqueue(self, val):
         self.s1.append(val)
 
+    # this method would dequeue the first stack by popping the first stack onto the second stack,
+    # then it returns the top values by popping the second stack
     def dequeue(self):
         if self.s2:
             return self.s2.pop()
-
         if self.s1:
             while self.s1:
                 self.s2.append(self.s1.pop())
             return self.s2.pop()
-
         return None
 
 
