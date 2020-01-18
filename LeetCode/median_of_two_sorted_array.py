83b966f5c6efccf308dd4af94cdf5f32d25c215a
kylemaa <kylephuma@gmail.com>
2020-01-17 19:30:34 -0800

    brute force solving for all examples

--- LeetCode/median_of_two_sorted_array.py
+++ LeetCode/median_of_two_sorted_array.py
@@ -6,10 +6,10 @@ import math
 
 
 def findMedianSortedArrays(nums1, nums2):
-    combine = nums1 + nums2
+    combine = sorted(nums1 + nums2)
     n = len(combine)
     if n % 2 != 0:
-        median_index = math.ceil(n/2)
+        median_index = math.floor(n/2)
         return combine[median_index]
     else:
         mid_point_1 = combine[int(n/2 - 1)]
@@ -18,6 +18,6 @@ def findMedianSortedArrays(nums1, nums2):
         return median
 
 
-nums1 = [1, 2]
-nums2 = [3, 4]
+nums1 = [1, 3]
+nums2 = [2]
 print(findMedianSortedArrays(nums1, nums2))
