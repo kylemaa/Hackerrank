d337847987ae09442788d848559569d9b12b73ab
kylemaa <kylephuma@gmail.com>
2020-01-19 19:01:13 -0800

    naive and stack approach for rearranging technique

--- LeetCode/reverse-int.py
+++ LeetCode/reverse-int.py
@@ -2,19 +2,17 @@ class Solution:
     # naive approach by converting x into string for string manipulation
     def reverseString(self, x: int) -> int:
         if x > 0:
-            ret = str(x)[::-1]
+            ret = int(str(x)[::-1])
         else:
             ret = -1 * int(str(x * -1)[::-1])
-        return ret
+        # for 32-bit overflow cases
+        return ((-2**31 <= ret) and (ret <= 2**31-1)) * ret
 
     # stack approach push each number and pop them into a reversed list
     def reverseStack(self, x: int) -> int:
-        if x > 0:
-            ret = str(x)[::-1]
-        else:
-            ret = -1 * int(str(x * -1)[::-1])
-        return ret
+        # convert the integer to string and have cases for negative number, 0 as last number
+        return None
 
 
-x = -123
+x = 120
 print(Solution().reverseString(x))
