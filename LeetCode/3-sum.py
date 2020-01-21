f266e6e139b8d25e5d5c997b9d74e4d199efd72d
kylemaa <kylephuma@gmail.com>
2020-01-20 17:44:08 -0800

    backtracking to pick the partial potential candidates

--- LeetCode/3-sum.py
+++ LeetCode/3-sum.py
@@ -1,9 +1,9 @@
 def threesum(nums):
-    nums.sort()
     res = []
-    # set up 3 some with fixed index
-    for i in range(len(nums)-2):
-        if i > 0 and nums[i] == nums[i-1]:
+    nums.sort()
+    # set up 3 some with fixed index i
+    for i in range(len(nums) - 2):
+        if i > 0 and nums[i] == nums[i - 1]:
             continue
         # two sum def of the sub array in nums
         # pointers at the beginning and the end of of nums
@@ -11,10 +11,10 @@ def threesum(nums):
         k = len(nums) - 1
         while j < k:
             if nums[i] + nums[j] + nums[k] == 0:
-                res.append(nums[i], nums[j], nums[k])
-                while j < k and nums[j] == nums[j+1]:
+                res.append([nums[i], nums[j], nums[k]])
+                while j < k and nums[j] == nums[j + 1]:
                     j += 1
-                while j < k and nums[k] == nums[k-1]:
+                while j < k and nums[k] == nums[k - 1]:
                     k -= 1
                 j += 1
                 k -= 1
@@ -22,7 +22,7 @@ def threesum(nums):
                 k -= 1
             else:
                 j += 1
-        return res
+    return res
 
 
 nums = [-1, 0, 1, 2, -1, -4]
