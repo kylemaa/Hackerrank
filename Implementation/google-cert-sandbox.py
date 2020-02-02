29284046c2c0aa0ca6f8b8aa01dfb6557ef637d9
kylemaa <kylephuma@gmail.com>
2020-02-01 22:38:42 -0800

    practicing for google IT cert

--- Implementation/google-cert-sandbox.py
+++ Implementation/google-cert-sandbox.py
@@ -1,18 +1,10 @@
-def pig_latin(text):
-    say = ""
-    # Separate the text into words
-    words = text.split(' ')
-    newwords = []
-    for word in words:
-        # Create the pig latin word and add it to the list
-
-        # Turn the list back into a phrase
-        newwords.append(word[1:len(word)]+word[0]+"ay")
-    for i in newwords:
-        say += " "+i
-    return say
+def email_list(domains):
+    emails = []
+    for domain in domains:
+        for user in domains[domain]:
+            emails.append(user+'@'+domain)
+    return(emails)
 
 
-print(pig_latin("hello how are you"))  # Should be "ellohay owhay reaay ouyay"
-# Should be "rogrammingpay niay ythonpay siay unfay"
-print(pig_latin("programming in python is fun"))
+print(email_list({"gmail.com": ["clark.kent", "diana.prince", "peter.parker"], "yahoo.com": [
+      "barbara.gordon", "jean.grey"], "hotmail.com": ["bruce.wayne"]}))
