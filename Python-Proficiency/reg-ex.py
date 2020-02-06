50e5f4318fc9f3e41eaf4618b9467c596950607e
kylemaa <kylephuma@gmail.com>
2020-02-05 17:59:32 -0800

    practicing for google IT cert

---
+++ Python-Proficiency/reg-ex.py
@@ -0,0 +1,14 @@
+import re
+
+
+def check_web_address(text):
+    pattern = "^[a-zA-Z_+.-]*\.[a-zA-Z]*$"
+    result = re.search(pattern, text)
+    return result != None
+
+
+print(check_web_address("gmail.com"))  # True
+print(check_web_address("www@google"))  # False
+print(check_web_address("www.Coursera.org"))  # True
+print(check_web_address("web-address.com/homepage"))  # False
+print(check_web_address("My_Favorite-Blog.US"))  # True
