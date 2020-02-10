3bd0e3b9c9e88f9959edc0ece752ff839e32915e
kylemaa <kylephuma@gmail.com>
2020-02-10 14:28:59 -0800

    Python Google Certificate practice

--- Python-Proficiency/log-analysis-using-regular-expressions.py
+++ Python-Proficiency/log-analysis-using-regular-expressions.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import re
 import operator
 import os
@@ -5,13 +6,23 @@ import sys
 
 
 def search_pattern(pattern, line):
+    result = re.search(pattern, line)
+    # egde case
+    if result == None:
+        return None
     # return only the captured group
-    result = re.findall(pattern, line)
-    return result
+    return result.group(1)
 
 
 # Parse the syslog.log file into two different dictionarys. Use for loops to count each occurence of each regex pattern above
 def parse_log(syslog):
+    # Dictionaries to keep count of each number and user interaction
+    error = {}
+    per_user = {}
+    info_pattern = r"ticky: INFO ([\w ]*)"
+    error_pattern = r"ticky: ERROR ([\w'? ]*)"
+    user_pattern = r"\((.*)\)"
+
     with open(syslog, 'r') as syslog:
         for line in syslog.readlines():
             res_err = search_pattern(error_pattern, line)
@@ -21,25 +32,28 @@ def parse_log(syslog):
             if res_user not in per_user:
                 per_user[res_user] = {'INFO': 0, 'ERROR': 0}
             # Check if the kind of error is already in the error dictionary
-            elif res_err not in error:
+            elif res_err != None and res_err not in error:
                 error[res_err] = 0
             # If there is an error
-            elif res_err != []:
+            elif res_err != None:
                 # increment the value of key 'ERROR' by 1 'INFO' in per_user dictionary
                 per_user[res_user]['ERROR'] += 1
                 # increment the type of that error in the error dictionart
                 error[res_err] += 1
             # If there is an info string only increment the value of key 'INFO' in per_user dictionary
-            elif res_info != []:
+            elif res_info != None:
                 per_user[res_user]['INFO'] += 1
         syslog.close()
+    return (error, per_user)
 
 
 # Generate report function to sort and write error into a csv report file
+
 def generate_error_report(dictionary, report_file):
     with open(report_file, "w+") as f:
-        for k in sorted(error.items(), key=operator.itemgetter(1), reverse=True):
-            f.write(str(k)+':'+str(dictionary[k])+'\n')
+        f.write("Error"+', ' + "Count" + '\n')
+        for k in sorted(dictionary.items(), key=operator.itemgetter(1), reverse=True):
+            f.write(str(k[0])+', '+str(k[1])+'\n')
         f.close()
 
 
@@ -54,25 +68,20 @@ def generate_user_report(dictionary, report_file):
 
 
 if __name__ == "__main__":
-    # Dictionaries to keep count of each number and user interaction
-    error = {}
-    per_user = {}
-    # Regex patterns required
-    info_pattern = r"ticky: INFO: ([\w ]*)"
-    error_pattern = r"ticky: ERROR: ([\w ]*)"
-    user_pattern = r"\((.*)\)"
     # Calling definitions
     syslog = sys.argv[1]
-    parse_log(syslog)
-    generate_error_report(error, '$HOME/error_message.csv')
-    generate_user_report(error, '$HOME/user_statistics.csv')
+    error, per_user = parse_log(syslog)
+    generate_error_report(
+        error, '/Users/KKyle/Hackerrank/Python-Proficiency/error_message.csv')
+    generate_user_report(
+        per_user, '/Users/KKyle/Hackerrank/Python-Proficiency/user_statistics.csv')
     sys.exit(0)
 
 '''
 # Save this file, then run the file by passing the path to syslog.log as a parameter to the script.
 
     sudo chmod +x log-analysis-regular-experessions.py
-    ./log-analysis-regular-experessions.py ~/data/syslog.log'
+    ./log-analysis-regular-experessions.py syslog.log'
 
 # To convert the error_message.csv into HTML file run the following command:
 
