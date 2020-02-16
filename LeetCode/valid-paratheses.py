02996aa6fb023009152f26fe39b4b6e8d7d55951
kylemaa <kylephuma@gmail.com>
2020-02-16 10:21:17 -0800

    Improved runtime of the code by checking the length of s first

--- LeetCode/valid-paratheses.py
+++ LeetCode/valid-paratheses.py
@@ -3,53 +3,25 @@
 
 def validParantheses(s):
     """This function returns True if a string contain valid parentheses else returns False"""
-
+    # Check if the length of the string is even
+    if len(s) % 2 != 0:
+        return False
+    # Declare a stack to match close/open brackets
     stack = []
+    # Dictionary with open brackets as key
+    par = {'(': ')', '[': ']', '{': '}'}
+    # Iterate the given string and append the open brackets
     for character in s:
-        # Push the open bracket into the stack
-        if character == '(':
-            stack.append('(')
-        if character == ')':
-            # If close bracket starts first then it is not valid
-            if len(stack) == 0:
-                return False
-            # If there is no matching bracket then it is not valid
-            if stack[-1] != '(':
-                return False
-            # If there is a matching bracket then pop the open bracket
+        if character in par.keys():
+            stack.append(character)
+        # Pop the open bracket if there is a matching close bracket
+        if len(stack) != 0 and character == par[stack[-1]]:
             stack.pop()
-
-        # Push the open bracket into the stack
-        if character == '{':
-            stack.append('{')
-        if character == '}':
-            # If close bracket starts first then it is not valid
-            if len(stack) == 0:
-                return False
-            # If there is no matching bracket then it is not valid
-            if stack[-1] != '{':
-                return False
-            # If there is a matching bracket then pop the open bracket
-            stack.pop()
-
-        # Push the open bracket into the stack
-        if character == '[':
-            stack.append('[')
-        if character == ']':
-            # If close bracket starts first then it is not valid
-            if len(stack) == 0:
-                return False
-            # If there is no matching bracket then it is not valid
-            if stack[-1] != '[':
-                return False
-            # If there is a matching bracket then pop the open bracket
-            stack.pop()
-
-    # After iterating through the string checks if the stack is empty
+    # Check again to see if all the open brackets are popped
     if len(stack) == 0:
         return True
     return False
 
 
-s = '[]{{}}()'
-print(validParantheses(s))
+s = '[]](){}'
+print(validParantheses(s))  # Should return False
