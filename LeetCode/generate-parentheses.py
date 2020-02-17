526f65199a3ea3f6ec30891719b22c3d244c7864
kylemaa <kylephuma@gmail.com>
2020-02-16 17:11:45 -0800

    Backtracking open brackets with recursion

--- LeetCode/generate-parentheses.py
+++ LeetCode/generate-parentheses.py
@@ -1,14 +1,17 @@
 def generateParentheses(n):
+    """This function call backtrack() to create valid parentheses"""
     res = []
 
     def backtrack(S, left, right):
         if len(S) == n*2:
             res.append(S)
             return
+        # left variable would increment from 0
         if left < n:
             backtrack(S + '(', left+1, right)
+        #  backtracking each incremental with closing brackets
         if left > right:
-            backtrack(S+')', left, right+1)
+            backtrack(S + ')', left, right+1)
     # Calling base case
     backtrack("", 0, 0)
     return res
