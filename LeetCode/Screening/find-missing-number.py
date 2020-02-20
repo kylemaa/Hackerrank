75075f0be8ee1488154901d522338c30d1c03cd0
kylemaa <kylephuma@gmail.com>
2020-02-19 17:31:40 -0800

    This function returns the smallest positive int that does not occur in given array

--- LeetCode/Screening/find-missing-number.py
+++ LeetCode/Screening/find-missing-number.py
@@ -1,23 +1,21 @@
-# Given an array A of N integers, return that smallest positive integer that does not occur in A. 
-import os
-def solution(arr):
-    m = max(arr)
-    if m < 1:
-        return 1
-    if len(arr)== 1:
-        if arr[0] == 1:
-            return 2
-        else:
-            return 1
-    l = [0]* m 
-    for i in range(len(arr)):
-        if arr[i]>0:
-            if l[arr[i]] !=1:
-                l[arr[i]] = 1
-    for j in range(len(l)):
-        if l[j] == 0:
-            return i+1
-    return i+2
+# Given an array A of N integers, return that smallest positive integer that does not occur in A.
 
-A = [1, 2, 5, 4]
-print(solution(A))
+
+def solution(A):
+    # Eliminate duplicates and sort the array
+    A = set(sorted(A))
+    number = 0
+    for i in A:
+        if i > 0 and i == number+1:
+            number = i
+        elif i != number + 1:
+            return number + 1
+    return number+1
+
+
+A = [1, 3, 6, 4, 1, 2]  # Expect 5
+
+B = [1, 2, 5, 4]  # Expect 3
+
+C = [1, 2, 3]  # Expect 4
+print(solution(C))
