d16b7fac11501e01343320cf34b73041401c87a5
Kyle Ma <kylephuma@gmail.com>
2020-03-26 19:25:30 -0700

    This function returns the max sum values of the subarrays

---
+++ LeetCode/max-subarray-int.py
@@ -0,0 +1,9 @@
+def max_subarray(nums):
+    for i in range(1, len(nums)):
+        if nums[i-1] > 0:
+            nums[i] += nums[i-1]
+        return max(nums)
+
+
+a = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+print(max_subarray(a))
